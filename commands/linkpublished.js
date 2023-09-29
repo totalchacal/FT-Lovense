30d62ee2d793d82eb559461b519073636d8054c1
totalchacal <33066120+totalchacal@users.noreply.github.com>
2023-09-29 18:43:32 +0200

    [FIX] started implementing embeds and getting all values properly to use later

--- commands/linkpublished.js
+++ commands/linkpublished.js
@@ -21,32 +21,55 @@ module.exports = {
               var normalResults = result.map((mysqlObj, index) => {
                 return Object.assign({}, mysqlObj);
             });
-            theFullList = "here are the people online who sent a link:\n\n"
+            theFullList = ""
             i = 0
 
             while(i != Object.keys(normalResults).length ){
-                console.log(result[i])
+                //console.log(result[i])
 
                 firstWho =  result[i].who
                 //const User = client.users.cache.get(firstWho);
                 firsttoys = result[i].toys
                 firstTime = result[i].linktime
                 firstlink = result[i].link
-                theFullList = theFullList+"who: "+firstWho+"\nToy(s): "+firsttoys+"\nLink: "+firstlink+"\nLink time: "+firstTime+"\n\n"
+                theFullList = theFullList+firstWho+" "+firsttoys+" "+firstlink+" "+firstTime+"\n"
                 i++;
             }
-
+            theFullestList = theFullList.split('\n')
+            i = 0
+            tiddiedupList = []
+            while(i < theFullestList.length-1) {
+                temp = theFullestList[i]
+                temp2 = temp.split(" ")
+                //console.log(temp2.length)
+                if(temp2.length == 6){
+                    tiddiedupList.push(temp2[0])
+                    tiddiedupList.push(temp2[1] +" " +temp2[3])
+                    tiddiedupList.push(temp2[4])
+                    tiddiedupList.push(temp2[5])
+                }
+                else{
+                    tiddiedupList.push(temp2[0])
+                    tiddiedupList.push(temp2[1])
+                    tiddiedupList.push(temp2[2])
+                    tiddiedupList.push(temp2[3])
+                }
+                i++
+            }
             const embeds = new EmbedBuilder()
-            .setTitle('help')
-            .setDescription("bot help menu")
+            .setTitle('Published link so far...')
             .setColor('Green')
-            .setAuthor({name:interaction.user.tag, iconURL: interaction.user.displayAvatarURL()})
             .addFields(
                 {
-                    name: theFullList,
-                    value: 'here is everything',
+                    name: tiddiedupList[0],
+                    value: tiddiedupList[1] +'\n'+tiddiedupList[2]+"\n"+tiddiedupList[3],
                     inline : true 
-                },)
+                },
+                {
+                    name: tiddiedupList[4],
+                    value: tiddiedupList[5] +'\n'+tiddiedupList[6]+"\n"+tiddiedupList[7],
+                    inline : true 
+                })
            // interaction.reply(theFullList)
             interaction.reply({
                 embeds : [embeds],
