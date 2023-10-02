65484da19b691c9b49ad8876a2315d49c413ddce
totalchacal <33066120+totalchacal@users.noreply.github.com>
2023-10-02 13:42:50 +0200

    [UPD]updated output + changed input fields and cleaned

--- commands/removelink.js
+++ commands/removelink.js
@@ -6,12 +6,12 @@ module.exports = {
     .setName('removelink')
     .setDescription('allow you to send a control link to the bot some dommes can use it')
     .addStringOption(option2 => option2 
-        .setName('string')
+        .setName('link')
         .setDescription(`link to send as a whole, copy paste the whole thing we'll take care of the rest`)
         .setRequired(true)
         ),
     async execute(interaction){
-      const text = interaction.options.getString('string')
+      const text = interaction.options.getString('link')
       var con = mysql.createConnection({
           host: "localhost",
           user: "root",
@@ -20,12 +20,10 @@ module.exports = {
       con.connect(function(err) {
           if (err) throw err;
           var sql = "DELETE FROM `links` WHERE `who` LIKE '"+ interaction.user.tag + "' AND `link` LIKE '"+ text +"'";
-          console.log(sql)
           con.query(sql, function (err, result) {
             if (err) throw err;
-            console.log(result)
             //interaction.reply("links were deleted")
-            interaction.reply({content :"links were deleted", ephemeral: true})
+            interaction.reply({content :"If the link exist it was deleted", ephemeral: true})
           });
           con.end()
         }); 
